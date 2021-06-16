490539177c5ab09990bf954fb68bdcd22b92582b
meslem-toufik-abdelrezak <58068200+meslem-toufik-abdelrezak@users.noreply.github.com>
2021-06-16 16:25:03 +0100

    login PSEUDOCODE

---
+++ profile.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once 'header.php';
+
+echo "hello user";
+
+    /*I NEED TO MAKE THE PSEUDOCODE BELLOW IN PHP USING POST METHOD*/
+// if(privilege == admin){
+//     echo "hello admin";
+// }
+
+// else{
+//     if (privilege == teacher){
+//         echo "hello teacher";
+//     }
+//     else {
+//         echo "hello a TALAB AT3ISS HHH";
+//     }
+
+// }
