215578131684e11890b52e1b14b25973998078a6
meslem-toufik-abdelrezak <58068200+meslem-toufik-abdelrezak@users.noreply.github.com>
2021-06-17 13:53:09 +0100

    path correcrtion from \ to /

--- footer.php
+++ footer.php
@@ -3,4 +3,4 @@
 </html>
 <!-- simple js doc with a simple purpse which is changing links color when your inside,
 you dont have to have it  11:20-->
-<script src="js\script.js" ></script>
+<script src="js/script.js" ></script>
