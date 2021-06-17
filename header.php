c267cbd770138e98a38bfb10a4db710c366241a9
meslem-toufik-abdelrezak <58068200+meslem-toufik-abdelrezak@users.noreply.github.com>
2021-06-17 13:48:28 +0100

    delete unuseful css link and add class to 1st <div> in <nav>

--- header.php
+++ header.php
@@ -7,8 +7,6 @@ session_start(); //when the user is loged in, will b login in all pages
     <head>
         <meta charset="utf-8">
         <title>homepage</title>
-        <!-- I can choose a font from googleapis the one below is an eg it is not crucial -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine"> 
         <!-- the 2 links below to 2 css files that i have to subscribe on patereron or get from 
         another source -->
         <link rel="stylesheet" href="css/reset.css" >  <!--reset the styling in all diff browsers -->
@@ -17,7 +15,7 @@ session_start(); //when the user is loged in, will b login in all pages
     <body>
                 
         <nav>
-            <div>
+            <div class="wrapper"> <!-- to wrapp evthg inside the header-->
             <!-- change the logo -->
                 <a href="index.php"><img src="img\homePageIcon.png" alt="Blogs logs" width="50"></a>
                 <ul> <!-- basic menu-->
