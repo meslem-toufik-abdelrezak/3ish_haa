4466a5cd9183be37f9b7c62d81c0b18a539151a0
meslem-toufik-abdelrezak <58068200+meslem-toufik-abdelrezak@users.noreply.github.com>
2021-06-17 16:28:28 +0100

    rewrite the comments in more proper way inside dbh file

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,8 +5,14 @@ $dBUsername="root";
 $dBPassword="";//when using xampp empty by defaul
 $dBName="loginsystem";//depends on what I call my DB inside phpMyAdmin
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName); //var connection --> pdo or mysqli used for OOP php --> mysqli used for procedural php
+/*$conn is var connection using  mysqli_connect() function
+Note:
+*pdo or mysqli used for OOP php
+*mysqli used for procedural php*/
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName); 
 
-if(!$conn) {// used to let us know if there's any sort of errors happening with the connection
-    die("Connection failed: " . mysqli_connect_error());//kill off whatever we're doing here, and spit out a msg
+if(!$conn) {//it's used to know if there's any sort of errors happening with the connection
+    //kill off whatever we're doing here, and spit out a msg
+    die("Connection failed: " . mysqli_connect_error());
+    //mysqli_connect_error() is a built in php function
 }
